7622335728c55ca5d3839bac8dd8069ca61ba7a0
Denis_Koval <den_hope@mail.ru>
2021-10-11 23:53:46 +0300

    feat: update script file

--- museum-dom/script.js
+++ museum-dom/script.js
@@ -1,7 +1,8 @@
 
 
 // console.log("Просьба оценку сразу не ставить, знаю что отстаю от графика, постораюсь доделать  к сроку дедлайна по проверки задания");
-console.log(`Ваша оценка `);
+console.log(`Просьба проверить в крайний день, отстаю от графика.
+Спасибо за понимание `);
 
 
 /*SLIDER */
@@ -75,85 +76,3 @@ squaresSlider.forEach( function (item, i) {
     }
   });
 });
-/*mouse swipe and touch */
-// const swipedetect = (el) => {
-//     let surface = el;
-// 	let startX = 0;
-// 	let startY = 0;
-// 	let distX = 0;
-// 	let distY = 0;
-// 	let startTime = 0;
-// 	let elapsedTime = 0;
-
-// 	let threshold = 150;
-// 	let restraint = 100;
-// 	let allowedTime = 1000;
-
-//     surface.addEventListener('mousedown', function(e){
-    
-// 		startX = e.pageX;
-// 		startY = e.pageY;
-// 		startTime = new Date().getTime();
-// 		e.preventDefault();
-// 	});
-
-// 	surface.addEventListener('mouseup', function(e){
-    
-// 		distX = e.pageX - startX;
-// 		distY = e.pageY - startY;
-// 		elapsedTime = new Date().getTime() - startTime;
-// 		if (elapsedTime <= allowedTime){
-// 			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
-// 				if ((distX > 0)) {
-// 					if (isEnabled) {
-// 						previousItem(currentItem);
-// 					}
-// 				} else {
-// 					if (isEnabled) {
-// 						nextItem(currentItem);
-// 					}
-// 				}
-// 			}
-// 		}
-// 		e.preventDefault();
-// 	});
-
-//     /* Touch swipe*/
-//     surface.addEventListener('touchstart', function(e){
-//         let touchObj = e.changedTouches[0];
-// 		startX = touchObj.pageX;
-// 		startY =touchObj.pageY;
-// 		startTime = new Date().getTime();
-// 		e.preventDefault();
-// 	});
-
-//     surface.addEventListener('touchend', function(e){
-//         e.preventDefault();
-//     });
-
-// 	surface.addEventListener('touchend', function(e){
-//         let touchObj = e.changedTouches[0];
-// 		distX = touchObj.pageX - startX;
-// 		distY = touchObj.pageY - startY;
-// 		elapsedTime = new Date().getTime() - startTime;
-// 		if (elapsedTime <= allowedTime){
-// 			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
-// 				if ((distX > 0)) {
-// 					if (isEnabled) {
-// 						previousItem(currentItem);
-// 					}
-// 				} else {
-// 					if (isEnabled) {
-// 						nextItem(currentItem);
-// 					}
-// 				}
-// 			}
-// 		}
-// 		e.preventDefault();
-// 	});
-
-	
-// }
-
-// let el = document.querySelector('.carousel');
-// swipedetect(el);
